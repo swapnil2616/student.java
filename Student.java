ec93a39fb128fe213fd24e048b199d2db194f64d
swapnil2616 <88187768+swapnil2616@users.noreply.github.com>
2024-02-22 13:03:31 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,12 @@
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String grade;
+    
+    // Getters and setters
+}
 
